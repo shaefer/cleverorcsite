0738348f0ca9717169f1ba9f67b056a8eb77e925
Daniel Shaefer <daniel.shaefer@hudl.com>
2018-04-23 12:15:14 -0500

    Added responsive navigation and styled it for both large and small screens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Nav.css';
 import { connect } from 'react-redux'
 import {keyPressHandler} from './action-creators'
 
@@ -28,17 +29,43 @@ class App extends Component {
     const { config, layout } = this.props;
     console.log(config.initialState);
     console.log(layout.initialLayout);
+
+    const menuItems = (
+      <ul>
+        <li class=""><a href="http://monsters.cleverorc.com">Pathfinder Monster Finder</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Test</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    );
+    const fullSizedNavBar = (
+      <nav class="nav center lg-only">
+        {menuItems}
+      </nav>
+    );
+    const condensedNavBar = (
+      <div class="flex-grid co_header fixed sm-only">
+        <div class="col center nopadding">
+          <nav class="nav center">
+            {menuItems}
+          </nav>
+        </div>
+      </div>
+    );
     return (
       <main>
       <header class="flex-grid co_header fixed">
         <div class="sidebar leftside"></div>
-        <div class="col center">
+        <div class="col center nopadding">
           <div class="mainTitle">Clever Orc</div>
           <div class="secondaryTitle">RPG Tools for Everyone</div>
+          {fullSizedNavBar}
         </div>
         <div class="sidebar rightside"></div>
       </header>
 
+     {condensedNavBar}
+      
       <div class="flex-grid co_content">
         <div class="col leftside">Left Col</div>
         <div class="col maincontent">Main Content</div>
