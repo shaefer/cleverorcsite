569622e45248c98ebf8eace4f6eb6e80b58ebd94
Daniel Shaefer <daniel.shaefer@hudl.com>
2018-04-26 20:41:53 -0500

    Updated Main sections to provide link to site and mailing list form

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const mailChimpForm = (
-<div id="mc_embed_signup">
+<div id="mc_embed_signup" class="carousel-text">
     <form action="https://cleverorc.us18.list-manage.com/subscribe/post?u=06964e8aa58cfc6c9065f9801&amp;id=d1c2c14583" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
-            <h2>Subscribe</h2>
             <div class="mc-field-group">
                 <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
                 <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL"/>
@@ -31,7 +30,12 @@ const slides = [
     {
       title: 'Pathfinder Monster Search',
       background: 'rgba(28,138,67,0.6)',
-      text: `Easily search for Pathfinder Monsters by name and view their full stat blocks. Currently includes Bestiary 1-4.`
+      jsx: (
+      <div class="carousel-text">
+        <span>Easily search for Pathfinder Monsters by name and view their full stat blocks. Currently includes Bestiary 1-4.</span>
+        <a href="http://monsters.cleverorc.com" class="link_button">Try Pathfinder Monster Search</a>
+      </div>
+      )
     },
     {
         title: 'About Us',
@@ -39,6 +43,7 @@ const slides = [
         text: `Clever Orc games is focused on delivering awesome gaming tools to players and gamemasters alike. We provide a robust offering of free tools and we operate by the Patreon support of our users and fans. `
     },
     {
+        title: 'Subscribe',
         background: 'rgba(28,138,67,0.6)',
         jsx: mailChimpForm
     }
