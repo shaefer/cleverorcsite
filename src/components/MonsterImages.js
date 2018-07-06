002e7b0c7e629b477e67ac8c2d8eeb7dd2393a24
Daniel Shaefer <shaefer@gmail.com>
2018-07-06 15:39:23 -0500

    Downsized images to better fit and save a bit of load time

--- src/components/MonsterImages.js
+++ src/components/MonsterImages.js
@@ -1,2 +1,2 @@
-export const orcKingUrl = '/images/orc_king__eric_quigley.png'
-export const phoenixUrl = '/images/phoenix__malcolm_mcclinton_trimmed.png'
+export const orcKingUrl = '/images/orc_king__eric_quigley_md.png'
+export const phoenixUrl = '/images/phoenix__malcolm_mcclinton_trimmed_mb.png'
